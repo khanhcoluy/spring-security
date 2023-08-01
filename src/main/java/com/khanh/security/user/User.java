aac29d333128eba89748e2c3325b0366d69be871
Khanh Nguyen <khanhcoluy@gmail.com>
2023-08-01 23:12:21 +0700

    - Add User Entity (change table name) - add builder annotate to help build object easier

---
+++ src/main/java/com/khanh/security/user/User.java
@@ -0,0 +1,27 @@
+package com.khanh.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+}
