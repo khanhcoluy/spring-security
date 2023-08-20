f4bc3ed904fc56a472f0a1fa456ec34d801ed82a
Khanh Nguyen <khanhcoluy@gmail.com>
2023-08-20 22:29:33 +0700

    - add isTokenValid, isTokenExpired, extractExpiration method

--- src/main/java/com/khanh/security/config/JwtService.java
+++ src/main/java/com/khanh/security/config/JwtService.java
@@ -35,6 +35,19 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     public String extractUsername(String jwtToken) {
         return extractClaim(jwtToken, Claims::getSubject); // i.e: Claims -> Claims.getSubject()
     }
