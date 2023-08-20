29232964aa3dcffe661e884e835a9d0f8be21a86
Khanh Nguyen <khanhcoluy@gmail.com>
2023-08-20 13:08:39 +0700

    - add jwt dependencies - add jwt ulti methods(extracClaims(), getSigningKey(), etc...)

--- src/main/java/com/khanh/security/config/JwtService.java
+++ src/main/java/com/khanh/security/config/JwtService.java
@@ -1,8 +1,37 @@
 package com.khanh.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
+    private static final String SECRET_KEY = "14F37665F1F3FC5433D4B385F6765";
 
     public String extractUsername(String jwtToken) {
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimExtractor) {
+        final Claims claims = extractAllClaims(token);
+        return claimExtractor.apply(claims);
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] decodedKey = Base64.getDecoder().decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(decodedKey);
     }
 }
