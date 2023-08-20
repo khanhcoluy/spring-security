b28b5246cc2a9c05c5d7ac70f373aeed84d5b9bc
Khanh Nguyen <khanhcoluy@gmail.com>
2023-08-20 17:54:37 +0700

    - add extractUsername method - add generateToken methods

--- src/main/java/com/khanh/security/config/JwtService.java
+++ src/main/java/com/khanh/security/config/JwtService.java
@@ -2,19 +2,41 @@ package com.khanh.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY = "14F37665F1F3FC5433D4B385F6765";
 
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        Date now = new Date(System.currentTimeMillis());
+        Date experationDate = new Date(System.currentTimeMillis() + 60 * 5);
+
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(experationDate)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
     public String extractUsername(String jwtToken) {
-        return null;
+        return extractClaim(jwtToken, Claims::getSubject); // i.e: Claims -> Claims.getSubject()
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimExtractor) {
