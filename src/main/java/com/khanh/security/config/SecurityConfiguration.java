93b6e78b381639e458167ee27a4aa3d9f0add7f9
Khanh Nguyen <khanhcoluy@gmail.com>
2023-08-29 23:06:44 +0700

    - implement demo controller to testing the spring security

--- src/main/java/com/khanh/security/config/SecurityConfiguration.java
+++ src/main/java/com/khanh/security/config/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
                 csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         authorize -> authorize
-                                .requestMatchers("")
+                                .requestMatchers("/api/v1/auth/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
